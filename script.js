96727a481b3969fbc349ae10f008b2533f71fa60
Iyesiam <zawahwi10@gmail.com>
2024-06-18 20:41:06 +0200

    fixed

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const pages = document.querySelectorAll('.page');
 let currentPage = 0;
 
+function updateZIndex() {
+    pages.forEach((page, index) => {
+        page.style.zIndex = (pages.length - Math.abs(currentPage - index)).toString();
+    });
+}
+
 document.getElementById('next').addEventListener('click', () => {
     if (currentPage < pages.length - 1) {
         pages[currentPage].style.transform = 'rotateY(-180deg)';
         currentPage++;
+        updateZIndex();
     }
 });
 
@@ -12,5 +19,9 @@ document.getElementById('prev').addEventListener('click', () => {
     if (currentPage > 0) {
         currentPage--;
         pages[currentPage].style.transform = 'rotateY(0deg)';
+        updateZIndex();
     }
 });
+
+// Initial zIndex setup
+updateZIndex();
