83bb25056ce291a5d85eecb024921bcedb248241
Iyesiam <zawahwi10@gmail.com>
2024-06-19 20:14:38 +0200

    tata

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ let currentPage = 0;
 
 document.getElementById('next').addEventListener('click', () => {
     if (currentPage < pages.length - 1) {
-        pages[currentPage].style.transform = 'rotateY(-180deg)';
-        currentPage++;
+        pages[currentPage].style.animation = 'rollOut 1s forwards';
+        pages[currentPage].addEventListener('animationend', () => {
+            pages[currentPage].classList.add('hidden');
+            currentPage++;
+            pages[currentPage].classList.remove('hidden');
+            pages[currentPage].style.animation = 'rollIn 1s forwards';
+        }, { once: true });
     }
 });
 
 document.getElementById('prev').addEventListener('click', () => {
     if (currentPage > 0) {
-        currentPage--;
-        pages[currentPage].style.transform = 'rotateY(0deg)';
+        pages[currentPage].style.animation = 'rollOut 1s reverse forwards';
+        pages[currentPage].addEventListener('animationend', () => {
+            pages[currentPage].classList.add('hidden');
+            currentPage--;
+            pages[currentPage].classList.remove('hidden');
+            pages[currentPage].style.animation = 'rollIn 1s reverse forwards';
+        }, { once: true });
     }
 });
